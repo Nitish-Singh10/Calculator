60826ffd791ee3cb35bc4fd95e75490fe4e49385
Nitish Singh <nitishsingh282016@gmail.com>
2023-12-06 22:59:03 +0530

    Written Some Code to Make Calculator But JS Remaning

---
+++ index.js
@@ -0,0 +1,10 @@
+function myfinction(operend1){
+    let x = operend1;
+    document.getElementById("ans").innerHTML= x;
+}
+
+
+function result(operend1,operend2){
+    let res = operend1 + operend2;
+    document.getElementById("ans").innerHTML= res;
+}
