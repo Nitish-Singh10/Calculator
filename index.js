f1acf88210cbf7914353484d514716d5b845f0b0
Nitish Singh <nitishsingh282016@gmail.com>
2023-12-15 22:46:09 +0530

    Tried to make some logic in JS

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function myfinction(operend1){
-    let x = operend1;
-    document.getElementById("ans").innerHTML= x;
-}
-
-function result(x,y){
-    let res = operend1 + operend2;
-    document.getElementById("ans").innerHTML= res;
+function myfunction(Operend){
+  console.log(Operend);
 }
